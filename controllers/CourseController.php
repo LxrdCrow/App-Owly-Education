39fbf65368ecfaf02e4138d6c65a0e95b47aacdc
Fede Yin <152525904+LxrdCrow@users.noreply.github.com>
2024-10-14 18:40:25 +0200

    CourseController.php

--- controllers/CourseController.php
+++ controllers/CourseController.php
@@ -8,38 +8,54 @@ class CourseController {
         $this->course = new Course();
     }
 
+    
     public function index() {
-        $courses = $this->course->getAllCourses();
+        $name = isset($_GET['name']) ? $_GET['name'] : null;
+        $available_slots = isset($_GET['available_slots']) && is_numeric($_GET['available_slots']) ? $_GET['available_slots'] : null;
+        $subject_ids = isset($_GET['subject_ids']) ? array_filter(explode(',', $_GET['subject_ids']), 'is_numeric') : null;
+        $limit = isset($_GET['limit']) && is_numeric($_GET['limit']) ? $_GET['limit'] : 10;  
+        $offset = isset($_GET['offset']) && is_numeric($_GET['offset']) ? $_GET['offset'] : 0;
+
+        
+        $courses = $this->course->getAllCourses($name, $available_slots, $subject_ids, $limit, $offset);
+        
+        
         echo json_encode($courses);
     }
 
+    
     public function store() {
         $data = json_decode(file_get_contents("php://input"), true);
         $name = $data['name'];
         $subjects = $data['subjects'];
-        $seats = $data['seats'];
+        $available_slots = $data['available_slots'];
 
-        if ($this->course->createCourse($name, $subjects, $seats)) {
+        
+        if ($this->course->createCourse($name, $subjects, $available_slots)) {
             echo json_encode(['message' => 'Create course successfully.']);
         } else {
             echo json_encode(['message' => 'Error creating course.']);
         }
     }
 
+    
     public function update($id) {
         $data = json_decode(file_get_contents("php://input"), true);
         $name = $data['name'];
         $subjects = $data['subjects'];
-        $seats = $data['seats'];
+        $available_slots = $data['available_slots'];
 
-        if ($this->course->updateCourse($id, $name, $subjects, $seats)) {
+        
+        if ($this->course->updateCourse($id, $name, $subjects, $available_slots)) {
             echo json_encode(['message' => 'Course updated successfully.']);
         } else {
             echo json_encode(['message' => 'Error updating course.']);
         }
     }
 
+    
     public function destroy($id) {
+        
         if ($this->course->deleteCourse($id)) {
             echo json_encode(['message' => 'Delete course successfully.']);
         } else {
@@ -48,4 +64,5 @@ class CourseController {
     }
 }
 ?>
+
 
