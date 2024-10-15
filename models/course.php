02d21f23fcf6f679912cca1c7f048b41539ae39b
Fede Yin <152525904+LxrdCrow@users.noreply.github.com>
2024-10-15 17:11:57 +0200

    course.php

--- models/course.php
+++ models/course.php
@@ -18,6 +18,51 @@ class Course {
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
+    public function getFilteredCourses($subject_ids = null, $name = null, $available_slots = null, $limit = 10, $offset = 0) {
+        $query = "SELECT c.* FROM " . $this->table . " c";
+        
+        if ($subject_ids) {
+            $query .= " INNER JOIN course_subject cs ON c.id = cs.course_id WHERE cs.subject_id IN (" . implode(',', array_fill(0, count($subject_ids), '?')) . ")";
+        } else {
+            $query .= " WHERE 1=1";
+        }
+    
+        if ($name) {
+            $query .= " AND c.name LIKE ?";
+        }
+    
+        if ($available_slots) {
+            $query .= " AND c.available_slots >= ?";
+        }
+    
+        $query .= " LIMIT ? OFFSET ?";
+    
+        $stmt = $this->conn->prepare($query);
+        
+
+        $params = [];
+        if ($subject_ids) {
+            foreach ($subject_ids as $id) {
+                $params[] = $id;
+            }
+        }
+        if ($name) {
+            $params[] = '%' . $name . '%';
+        }
+        if ($available_slots) {
+            $params[] = $available_slots;
+        }
+    
+        
+        $params[] = $limit;
+        $params[] = $offset;
+    
+        $stmt->execute($params);
+    
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+
     
     public function createCourse($name, $subjects, $seats) {
         $query = "INSERT INTO " . $this->table . " (name, subjects, seats) VALUES (:name, :subjects, :seats)";
